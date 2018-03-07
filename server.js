046a3cefb572dc75bdce7f4118d69916fafc1eda
Gaurav Parmar <gsp17feb@gmail.com>
2018-03-08 00:18:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express')
 var ws = require('./ws')
 
-var app = express()
+var app = express(),
+port = parseInt(process.env.PORT || config.server.port, 10)
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/ws.html');
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log('Example app listening on port ! '+port)
 })
